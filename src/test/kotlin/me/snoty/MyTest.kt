4bbc53b202b49423e0ec777b09042cf0da638b2e
SIMULATAN <simulatan2319@gmail.com>
2024-03-29 10:14:47 +0100

    Initial Project

---
+++ src/test/kotlin/me/snoty/MyTest.kt
@@ -0,0 +1,17 @@
+package me.snoty
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class MyTest {
+	@Test
+	fun failedTest() {
+		assertEquals(1, 2)
+	}
+
+	@Test
+	fun workingTest() {
+		assertEquals(1, 1)
+	}
+}
